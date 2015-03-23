1307ddfbfc4492c41c8b90686d6f40bc87f849a6
Pete Hodgson <git@thepete.net>
2015-03-23 08:03:54 -0700

    Use Immutable for app state

--- js/createPropMutator.js
+++ js/createPropMutator.js
@@ -1,16 +1,7 @@
 module.exports = createPropMutator;
 
 function createPropMutator(propertyName,mutator){
-  return function(obj){
-    // TODO: a proper fully immutable impl.
-    const mutatedObj = {}
-    for (const prop in obj) {
-      if( prop === propertyName ){
-        mutatedObj[prop] = mutator(obj[prop]);
-      }else{
-        mutatedObj[prop] = obj[prop];
-      }
-    }
-    return mutatedObj;
+  return function(immutable){
+    return immutable.update(propertyName,mutator);
   }
 }

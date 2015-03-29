7d5299a4457c85d6cf02480a21ffb178f6362644
Pete Hodgson <git@thepete.net>
2015-03-28 17:39:25 -0700

    delete dead code

--- js/awaken.js
+++ js/awaken.js
@@ -1,6 +1,5 @@
 module.exports = {
   createPropMutator: require('./createPropMutator'),
-  createSubstateUpdater: require('./createSubstateUpdater'),
   namespacedNotify: require('./namespacedNotify'),
   boot: require('./boot'),
   h: require('virtual-dom/virtual-hyperscript')

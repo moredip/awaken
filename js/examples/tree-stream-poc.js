36c841f40efa696bcd1d4da8236acad6f2a8457d
Pete Hodgson <git@thepete.net>
2015-04-20 08:41:54 -0700

    counter wired up as a tree stream

--- js/examples/tree-stream-poc.js
+++ js/examples/tree-stream-poc.js
@@ -1,16 +1,88 @@
-const h = require('virtual-dom/virtual-hyperscript'),
-      Bacon = require('baconjs');
+const h = Awaken.h,
+    createPropMutator = Awaken.createPropMutator;
+
+const Bacon = require('baconjs'),
+      Immutable = require('immutable');
 
 const createTreeStreamForContainer = require('../treeStream');
 
 const appContainer = document.getElementsByTagName('main')[0];
-const treeStream = createTreeStreamForContainer(appContainer);
+const treeRealizationStream = createTreeStreamForContainer(appContainer);
+
+function render(appState, notifyFn){
+
+  function onClickUp(){
+    notifyFn('up');
+  };
+
+  function onClickDown(){
+    notifyFn('down');
+  };
+
+  const content = 'count: '+appState.count;
+  return h(
+    'section',
+    [
+    h(
+      'p',
+      content
+    ),
+    h(
+      'button',
+      { onclick: onClickUp },
+      'UP'
+    ),
+    h(
+      'button',
+      { onclick: onClickDown },
+      'DOWN'
+    )
+    ]
+  );
+}
+
+function incrOne(x){ return x+1; }
+function decrOne(x){ return x-1; }
+const upper = createPropMutator('count', incrOne )
+const downer = createPropMutator('count', decrOne )
+
+const reactors = {
+  up: upper,
+  down: downer
+};
+
+function react(notification){
+  return reactors[notification];
+}
+
+const notificationStream = new Bacon.Bus(),
+      n = function(x){ notificationStream.push(x); };
+
+const initialState = { count:0 };
+
+const reactionStream = notificationStream
+  .log('notification:')
+  .map(react);
 
 
+function appStateTransformer(previousState,transformFn){
+  if( transformFn ){
+    return transformFn(previousState);
+  }else{
+    return previousState;
+  }
+}
 
-const appStateStream = Bacon.repeatedly(500,"abcdefg".split(''))
-  .map( function(x){
-    return h('h1', 'letter '+x);
+const transformedAppStates = reactionStream
+    .scan( Immutable.fromJS(initialState), appStateTransformer )
+
+const treeStream = transformedAppStates
+  .map( function(appState){
+    return render(appState.toJS(),n);
   });
 
-treeStream.plug(appStateStream);
+
+treeRealizationStream.plug( treeStream );
+
+notificationStream.push('startup');
+
--- js/treeStream.js
+++ js/treeStream.js
@@ -12,7 +12,7 @@ function createTreeStreamForContainer(container){
   const initialRealizer = createInitialRealizerFn( container );
 
   treeStream
-    .log('realizing tree:')
+    //.log('realizing tree:')
     .scan(initialRealizer,applyRealizerToTree)
     .onValue(); // this is needed to create a pull through the stream.
 
